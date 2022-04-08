7acd3036b1c48ef130ab714c8dab31c02e7e46c4
dianazevedoferreira <40640339+dianazevedoferreira@users.noreply.github.com>
2022-04-08 14:02:15 +0100

    Add PL06 & PL07

---
+++ PL06 & PL07/medicamentos.sql
@@ -0,0 +1,17 @@
+INSERT INTO farmacos(id_farmaco,nome, descricao) VALUES(
+1000, 'Acarbose', 'Comprimido revestido por película hipoglicemiante');
+
+INSERT INTO farmacos(nome,descricao) VALUES
+('Acarbose', 'Comprimido revestido por película hipoglicemiante'),
+('Amoxicilina', 'Comprimido revestido por película antibiótico utilizado para tratar diversas infecções bacterianas'),
+('Bicarbonato de sódio', 'Solução injetável alcalinizante'),
+('Betametasona', 'Pomada corticóide com potente ação anti-inflamatória e antialérgica'),
+('Ferro', 'Solução injetável para reposição de ferro'),
+('Ibuprofeno', 'Comprimido revestido por película anti-inflamatório não esteroidal'),
+('Insulina glulisina', 'Solução injetável antidiabética'),
+('Levetiracetam', 'Comprimido revestido por película antiepiletico'),
+('Levotiroxina', 'Comprimido de hormona tiroideia sintética usado no tratamento de reposição hormonal'),
+('Omeprazol', 'Cápsula gastrorresistente'),
+('Oxitocina', 'Solução para pulverização nasal'),
+('Pantoprazol', 'Comprimido gastrorresistente'),
+('Paracetamol', 'Comprimido revestido por película analgésico');
